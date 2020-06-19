222d9819e4fa87f0fd7e8e5218b507d1b0e9f9dc
KD Circuits <kevin@kdcircuits.com>
2020-06-19 00:09:13 -0400

    more youtube testing

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+sys.path.append(os.path.abspath('exts/sphinxcontribyoutube/sphinxcontrib'))
 # -- Project information -----------------------------------------------------
 
 project = u'trigBoard\nVersion 8'
@@ -38,8 +38,7 @@ release = u'0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['youtube']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
